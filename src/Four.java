fcc48657c03eb1edc58ebb63d2817e933d84f8aa
Marco Alvarez <blazingjove@gmail.com>
2024-05-04 17:58:31 -0500

    solution to four

--- src/Four.java
+++ src/Four.java
@@ -1,32 +1,22 @@
-/*
-The objective is to return all pairs of integers from a given array of integers that have a difference of 2.
+/*Given an array of integers your solution should find the smallest integer.
 
-The result array should be sorted in ascending order of values.
+For example:
 
-Assume there are no duplicate integers in the array. The order of the integers in the input array should not matter.
-Examples
-
-        [1, 2, 3, 4]  should return [[1, 3], [2, 4]]
-
-        [4, 1, 2, 3]  should also return [[1, 3], [2, 4]]
-
-        [1, 23, 3, 4, 7] should return [[1, 3]]
-
-        [4, 3, 1, 5, 6] should return [[1, 3], [3, 5], [4, 6]]
-*/
-
-
-import java.util.Arrays;
-import java.util.List;
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.*/
 
 public class Four {
-    public static int[][] twosDifference(int[] array) {
-        //declaring and assigning values to vars
-        int length = array.length;
+    public static int findSmallestInt(int[] args) {
+        int lowest = args[0];
 
-        //sorting array values from smallest to largest
-        list sortedList = Arrays.asList(Arrays.sort(array));
+        for (int i = 0; (i < args.length-1); i++) {
+            if (lowest > args[i+1]){
+                lowest = args[i+1];
+                System.out.println(lowest);
+            }
 
-        System.out.println(List.toString(sortedList));
+        }
+        return lowest;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 //main used to test problems
 
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Four.twosDifference(new int[]{4, 3, 1, 5, 6}));
+        int ans;
+        ans = Four.findSmallestInt(new int[]{-59998,-970, 56, -2, 12, -8978,-1039999});
+        System.out.println(ans);
     }
 }
-
